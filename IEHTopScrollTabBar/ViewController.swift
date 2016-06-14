3cf450184bf9653aa2ec741dd7cf84f755bc0ea7
“Ismail <i.eh@hostelworld.com>
2016-06-14 12:28:31 +0100

    Initial Commit

---
+++ IEHTopScrollTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IEHTopScrollTabBar
+//
+//  Created by ismail el habbash on 14/06/2016.
+//  Copyright © 2016 ismail el habbash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
