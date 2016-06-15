f16e49a3854ff9d581c56d3f96723c16da053599
“Ismail <i.eh@hostelworld.com>
2016-06-15 15:44:50 +0100

    works fine with needs improvements in scrolling

---
+++ IEHTopScrollTabBar/TopScrollTabBar.swift
@@ -0,0 +1,150 @@
+//
+//  TopScrollTabBar.swift
+//  IEHTopScrollTabBar
+//
+//  Created by ismail el habbash on 14/06/2016.
+//  Copyright © 2016 ismail el habbash. All rights reserved.
+//
+
+import UIKit
+
+class TopScrollTabBar: UIScrollView {
+
+    let height:CGFloat = 44.0
+    let buttonPadding:CGFloat = 10.0
+    private var selectedButton:UIButton = UIButton() {
+        didSet {
+            selectedButton.backgroundColor = selectedButton.titleColorForState(.Normal)
+            selectedButton.setTitleColor(UIColor.whiteColor(), forState: .Selected)
+            selectedButton.backgroundColor = selectedButton.titleColorForState(.Normal)
+            self.backgroundColor = selectedButton.titleColorForState(.Normal)
+        }
+    }
+
+    private lazy var topScrollBarButtons = [UIButton]()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //        showsHorizontalScrollIndicator = false
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    convenience init(inView:UIView, items:[TopScrollTabBarItem]) {
+        let yPoint = UIApplication.sharedApplication().statusBarFrame.height
+        let rect = CGRect(x: inView.bounds.origin.x , y: yPoint, width: inView.frame.width, height: 44)
+        self.init(frame: rect)
+
+        addScrollItems(items)
+        intialiseFirstLook()
+    }
+
+    private func intialiseFirstLook() {
+        showsHorizontalScrollIndicator = false
+
+        topScrollBarButtons.first?.selected = true
+        selectedButton = topScrollBarButtons.first ?? UIButton()
+        self.backgroundColor = selectedButton.titleColorForState(.Normal)
+    }
+    private func addScrollItems(items:[TopScrollTabBarItem]) {
+        // initialise the top scrollbar items
+        var accumulatedWidth:CGFloat = 0.0
+
+        for item in items {
+            let button = createButtonForItem(item)
+            let buttonWidth = button.frame.size.width
+            accumulatedWidth += buttonWidth
+
+            button.center = CGPoint(x:accumulatedWidth - buttonWidth/2, y: button.center.y)
+
+            topScrollBarButtons.append(button)
+            addSubview(button)
+        }
+        self.contentSize = CGSize(width: accumulatedWidth, height: height)
+    }
+
+    private func createButtonForItem(item:TopScrollTabBarItem) -> UIButton {
+        let button = UIButton(frame: CGRect.zero)
+        button.setTitle(item.title, forState: .Normal)
+
+        button.setTitleColor(item.color, forState: .Normal)
+
+        button.backgroundColor = UIColor.whiteColor()
+
+        button.setTitleColor(item.color.colorWithAlphaComponent(0.4) , forState: .Highlighted)
+
+        button.sizeToFit()
+        let buttonFrame = button.frame
+        button.frame = CGRect(x: 0.0, y: 0.0, width: buttonFrame.width + 2*buttonPadding, height: self.frame.size.height)
+        button.addTarget(self, action: #selector(TopScrollTabBar.buttonTapped(_:)), forControlEvents: .TouchUpInside)
+
+        return button
+    }
+
+    func buttonTapped(sender: UIButton) {
+        focusOnButton(sender)
+        selectedButton.selected = false
+        selectedButton = sender
+        for b in topScrollBarButtons {
+            if b == selectedButton {
+                selectedButton.selected = true
+            } else {
+                b.backgroundColor = UIColor.whiteColor()
+            }
+        }
+    }
+
+    private func focusOnButton(button:UIButton) {
+        let leftSpace = button.frame.origin.x
+        let rightSpace = self.contentSize.width - leftSpace
+        let centerOfScreen = bounds.size.width/2
+        let buttonWidth = button.frame.size.width
+        var moveableDistance:CGPoint = CGPoint.zero
+        let maxOffsetX = contentSize.width - frame.size.width
+        print("rightSpace = \(rightSpace) leftSpace = \(leftSpace) contentOffset = \(contentOffset.x) centerOfScreen = \(centerOfScreen) self.bounds.size.width = \(self.bounds.size.width) contentSize.width = \(contentSize.width) maxOffsetX = \(maxOffsetX)")
+
+        if (leftSpace < centerOfScreen) {
+            return
+        }
+        else if (leftSpace < contentSize.width/2 && leftSpace > centerOfScreen) {
+            let availableSpace = leftSpace - centerOfScreen
+            print("left:availableSpace = \(availableSpace)")
+            moveableDistance.x = availableSpace + buttonWidth/2
+        } else if (rightSpace < centerOfScreen) {
+            let availableSpace =  centerOfScreen - rightSpace
+            print("right:availableSpace = \(availableSpace)")
+            moveableDistance.x = availableSpace + buttonWidth
+
+        }
+            //center the item
+        else {
+            print("in the else")
+            //            moveableDistance.x = centerOfScreen
+        }
+        setContentOffset(moveableDistance,animated: true)
+    }
+}
+
+class TopScrollTabBarItem {
+    var title:String
+    var color:UIColor
+
+    init(title:String, color:UIColor) {
+        self.title = title
+        self.color = color
+    }
+}
+
+//extension UIImage {
+//    class func imageWithColor(color: UIColor) -> UIImage {
+//        let rect: CGRect = CGRectMake(0.0, 0.0, 1.0, 1.0)
+//        UIGraphicsBeginImageContext(rect.size)
+//        let context: CGContextRef = UIGraphicsGetCurrentContext()!
+//        CGContextSetFillColorWithColor(context, color.CGColor)
+//        CGContextFillRect(context, rect)
+//        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+//        UIGraphicsEndImageContext()
+//        return image
+//    }
+//}
--- IEHTopScrollTabBar/ViewController.swift
+++ IEHTopScrollTabBar/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let item1 = TopScrollTabBarItem(title: "Hot", color: UIColor(red: 2/255, green: 174/255, blue: 220/255, alpha: 1))
+        let item2 = TopScrollTabBarItem(title: "Social", color: UIColor(red: 255/255, green: 212/255, blue: 100/255, alpha: 1))
+        let item3 = TopScrollTabBarItem(title: "Politics", color: UIColor(red: 18/255, green: 83/255, blue: 164/255, alpha: 1))
+        let item4 = TopScrollTabBarItem(title: "Business", color: UIColor(red: 47/255, green: 205/255, blue: 180/255, alpha: 1))
+        let item5 = TopScrollTabBarItem(title: "Entertaiment", color: UIColor(red: 251/255, green: 168/255, blue: 72/255, alpha: 1))
+        let item6 = TopScrollTabBarItem(title: "Tech", color: UIColor(red: 12/255, green: 85/255, blue: 93/255, alpha: 1))
+        let item7 = TopScrollTabBarItem(title: "Media", color: UIColor(red: 2/255, green: 150/255, blue: 200/255, alpha: 1))
+        let item8 = TopScrollTabBarItem(title: "Comedy", color: UIColor(red: 255/255, green: 83/255, blue: 200/255, alpha: 1))
+        let topScrollTabBarItems = [item1, item2, item3, item4, item5, item6, item7, item8]
+
+        let topScrollTabBar = TopScrollTabBar(inView: view, items: topScrollTabBarItems)
+
+        view.addSubview(topScrollTabBar)
     }
 
     override func didReceiveMemoryWarning() {
